0905498235affeca078ec27016c30f1581765a30
romajs <joseromualdoneto@gmail.com>
2016-04-09 22:56:16 -0300

    scripts

---
+++ scripts/angularjs-dx-modules/pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+CURRENT_DIR="$(dirname $(readlink -f $0))/"
+PROJECT_DIR=$CURRENT_DIR../../
+
+source $CURRENT_DIR/.env
+
+cd $PROJECT_DIR
+git subtree pull --prefix=$SUBTREE_PATH $GIT_REPO_URL master --squash
+cd -
